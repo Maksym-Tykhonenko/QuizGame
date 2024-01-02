ec45b46bf8d1024aad36ecdd7ee16fbb453940a5
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-01-02 13:33:57 +0200

    init commit

---
+++ quizGame/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+const App=()=> {
+ 
+  return (
+    <View style={{ flex:1 , alignItems: 'center', justifyContent: 'center'}}>
+      <Text>QUIZ GAME !!!!!</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
