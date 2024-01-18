fdbc408685875a16bc477ef7137a93ea030079ed
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-01-18 15:44:19 +0200

    save

--- quizGame/App.js
+++ quizGame/App.js
@@ -15,6 +15,27 @@ const App=()=> {
   );
 }
 
+{/**
+<WebView
+  originWhitelist={['*']}
+  // Інші властивості WebView
+  onShouldStartLoadWithRequest={(event) => {
+    const { url } = event;
+    if (url.startsWith('mailto:')) {
+      // Якщо це посилання на пошту (mailto), відкрийте його в іншому додатку
+      Linking.openURL(url);
+      return false; // Повертаємо false, щоб заборонити завантаження URL в WebView
+    } else if (url.includes('ваш_префікс_для_криптогаманця')) {
+      // Якщо це посилання на криптогаманець, відкрийте його в іншому додатку
+      Linking.openURL(url);
+      return false; // Повертаємо false, щоб заборонити завантаження URL в WebView
+    } else {
+      return true; // Всі інші URL можна завантажувати в WebView
+    }
+  }}
+/>
+
+*/}
 
 
 export default App;
